843cd4b746c30af6259737291238c0c60da78761
kiran-91 <deshpandekiran007kd@gmail.com>
2025-01-17 20:21:16 +0530

    adding files

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st 
+
+def main():
+    st.set_page_config(page_title="Information Retrieval Tool", page_icon="📑🔖")
+    
+    
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Information Retrieval Tool",
+    version= "0.1",
+    author= "Kiran Deshpande",
+    author_email= "deshpandekiran007kd@gmail.com",
+    packages= find_packages(),
+    install_requires= []
+    
+    
+    
+    
+    
+    
+    
+)
